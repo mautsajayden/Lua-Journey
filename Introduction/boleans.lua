d407f8d6c99e6217fc97fe668570533813be3abf
Jayden Mautsa <163717439+mautsajayden@users.noreply.github.com>
2025-12-22 23:53:50 -0500

    intro
    learning how to code in lua

---
+++ Introduction/boleans.lua
@@ -0,0 +1,8 @@
+--[[Create a variable named isReady and assign it the boolean value true. 
+Then print the value of this variable.
+]]
+
+-- TODO: Write your code here
+
+isReady = true
+print(isReady)
