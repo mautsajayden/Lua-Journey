fc9cf334f25ce337aadfc738ba05d1392d4b2864
Jayden Mautsa <163717439+mautsajayden@users.noreply.github.com>
2025-12-22 23:54:07 -0500

    Create characterProfile_Challange.lua
    learning how to use data types

---
+++ Introduction/characterProfile_Challange.lua
@@ -0,0 +1,13 @@
+-- TODO: Create the character profile variables here
+
+characterName = "Warrior"
+characterLevel = 12
+characterHealth = 85
+isHero = true
+
+-- TODO: Print each variable on a separate line
+
+print(characterName)
+print(characterLevel)
+print(characterHealth)
+print(isHero)
