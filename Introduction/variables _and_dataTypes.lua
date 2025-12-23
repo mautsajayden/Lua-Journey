ceee95532dadca63dc65c1d1d5fc33e0b219a75d
Jayden Mautsa <163717439+mautsajayden@users.noreply.github.com>
2025-12-22 23:54:39 -0500

    Create variables _and_dataTypes.lua

---
+++ Introduction/variables _and_dataTypes.lua
@@ -0,0 +1,14 @@
+--[[Create two variables to store numeric information for a game character.
+ First, create a variable called playerScore and assign it the value 2500. 
+ Then, create a variable called gameVersion and assign it the value 1.3. \
+ Finally, print both variables to display their values.]]
+
+
+-- TODO: Write your code here
+
+playerScore = 2500
+gameVersion = 1.3
+
+print(playerScore)
+print()
+print(gameVersion)
