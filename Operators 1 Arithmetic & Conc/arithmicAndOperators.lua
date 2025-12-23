a891fb0bc1ffd57899239e62753cdca07fecbc75
Jayden Mautsa <163717439+mautsajayden@users.noreply.github.com>
2025-12-22 23:54:46 -0500

    Create arithmicAndOperators.lua

---
+++ Operators 1 Arithmetic & Conc/arithmicAndOperators.lua
@@ -0,0 +1,9 @@
+-- Create variables for item prices
+-- TODO: Write your code here
+swordPrice = 25
+shieldPrice = 18
+-- Calculate total cost
+-- TODO: Write your code here
+totalCost = swordPrice + shieldPrice
+-- Print the total cost
+print(totalCost)
