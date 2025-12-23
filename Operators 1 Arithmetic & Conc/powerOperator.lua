d54e98e173d655b16d50adf3476dc940e9f0bce2
Jayden Mautsa <163717439+mautsajayden@users.noreply.github.com>
2025-12-22 23:55:01 -0500

    Create powerOperator.lua

---
+++ Operators 1 Arithmetic & Conc/powerOperator.lua
@@ -0,0 +1,13 @@
+--[[Calculate the area of a square garden plot using the exponentiation operator. 
+Create a variable called sideLength and assign it the value 7. Then use the exponentiation 
+operator to calculate the area by raising the side length to the power of 2, and store this 
+result in a variable called area. Print the calculated area.]]
+
+
+-- Create variables for the garden plot calculation
+local sideLength = 7
+
+-- TODO: Write your code here to calculate the area using exponentiation
+area = sideLength ^ 2
+-- Print the calculated area
+print(area)
