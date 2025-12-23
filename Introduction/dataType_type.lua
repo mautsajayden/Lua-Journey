2737f57d6e8f7a46840d668c3ca565d4ed3863c3
Jayden Mautsa <163717439+mautsajayden@users.noreply.github.com>
2025-12-22 23:54:23 -0500

    Create dataType_type.lua
    using the variable typa

---
+++ Introduction/dataType_type.lua
@@ -0,0 +1,18 @@
+--[[You are provided with several pre-defined variables containing different types of data. Use the type() 
+function to determine and print the data type of each variable.
+
+The variables are:]]
+
+
+-- Pre-defined variables
+mysteryValue1 = 42
+mysteryValue2 = "Adventure"
+mysteryValue3 = false
+mysteryValue4 = nil
+
+-- TODO: Write your code here to determine and print the type of each variable
+
+print(type(mysteryValue1))
+print(type(mysteryValue2))
+print(type(mysteryValue3))
+print(type(mysteryValue4))
