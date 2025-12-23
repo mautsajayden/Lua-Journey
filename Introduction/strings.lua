fdb0ead195430042f0f8b52cf25d4b62e73a8a3d
Jayden Mautsa <163717439+mautsajayden@users.noreply.github.com>
2025-12-22 23:54:32 -0500

    Create strings.lua

---
+++ Introduction/strings.lua
@@ -0,0 +1,8 @@
+--[[Create a variable named playerName and assign it the string value Hero. 
+Then print the value of this variable to display the character's name.]]
+
+
+-- TODO: Write your code here
+
+playerName = "Hero"
+print(playerName)
