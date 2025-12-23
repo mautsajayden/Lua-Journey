566a96d0e8e122fa876df4ecbf5bde5a51f90213
Jayden Mautsa <163717439+mautsajayden@users.noreply.github.com>
2025-12-22 23:54:42 -0500

    Create variables.lua

---
+++ Introduction/variables.lua
@@ -0,0 +1,8 @@
+-- TODO: Write your code here
+
+--[[Create a variable called gameLevel and assign it the number 5. 
+Then print the value of this variable.]]
+
+gameLevel = 5 --variable that contains the value 5 
+
+print(gameLevel) --prints out the value inside the variable 
