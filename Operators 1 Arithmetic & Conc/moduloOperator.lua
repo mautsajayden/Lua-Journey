dad02800d4e51068983dcbfbcc62ecbc80a775c3
Jayden Mautsa <163717439+mautsajayden@users.noreply.github.com>
2025-12-22 23:54:54 -0500

    Create moduloOperator.lua

---
+++ Operators 1 Arithmetic & Conc/moduloOperator.lua
@@ -0,0 +1,13 @@
+--[[Check if a player's score is a multiple of 10 using the modulo operator. 
+Create a variable called playerScore and assign it the value 85. Then use the
+ modulo operator to find the remainder when dividing the score by 10, and store 
+ this result in a variable called remainder. Print the remainder value.]]
+
+
+-- Create the playerScore variable and assign it the value 85
+local playerScore = 85
+
+-- TODO: Write your code here to calculate the remainder using modulo operator
+remainder = playerScore % 10
+-- Print the remainder
+print(remainder)
