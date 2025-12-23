11f48e112338adf73afa5e735a4ebf42f3b7cfdc
Jayden Mautsa <163717439+mautsajayden@users.noreply.github.com>
2025-12-22 23:54:36 -0500

    Create valueNil.lua

---
+++ Introduction/valueNil.lua
@@ -0,0 +1,8 @@
+
+--[[Create a variable called playerStatus without assigning any value to it. 
+Then print this variable to see what Lua assigns by default.]]
+
+-- TODO: Write your code here
+
+local playerStatus 
+print(playerStatus)
